37ff3ecca0e398660cd4f7503fe0fee208804946
Victor Engmark <victor.engmark@gmail.com>
2014-02-08 17:05:15 +0000

    Use 0-based indexes internally

--- vcard4/test_vcard4.py
+++ vcard4/test_vcard4.py
@@ -19,14 +19,14 @@ class TestRFC6350(unittest.TestCase):
         self.assertRaises(VcardError, split_lines, MINIMAL_VCARD_LINES_SEPARATED_BY_REVERSED_LINE_BREAK)
 
     def test_individual_lines_within_vcard_are_delimited_by_the_rfc5322_line_break_error(self):
-        first_invalid_character = len(MINIMAL_VCARD_LINES[0]) + 1
+        first_invalid_character_index = len(MINIMAL_VCARD_LINES[0])
         with self.assertRaises(VcardError) as error:
             split_lines(MINIMAL_VCARD_LINES_SEPARATED_BY_CARRIAGE_RETURN)
 
         self.assertEqual(
             error.exception.message,
-            "Invalid line delimiter at character {0}".format(first_invalid_character))
-        self.assertEqual(error.exception.character_index, first_invalid_character)
+            "Invalid line delimiter at character {0}".format(first_invalid_character_index + 1))
+        self.assertEqual(error.exception.character_index, first_invalid_character_index)
 
 
 if __name__ == '__main__':
--- vcard4/vcard4.py
+++ vcard4/vcard4.py
@@ -9,12 +9,12 @@ def split_lines(text):
     for line in lines:
         invalid_newlines = INVALID_LINE_BREAKS_MATCHER.search(line)
         if invalid_newlines is not None:
-            raise VcardError("Invalid line delimiter", invalid_newlines.end())
+            raise VcardError("Invalid line delimiter", invalid_newlines.start())
     return lines
 
 
 class VcardError(Exception):
-    def __init__(self, message, character):
+    def __init__(self, message, character_index):
         super(VcardError, self).__init__(message)
-        self.character_index = character
-        self.message = "{0} at character {1}".format(message, self.character_index)
+        self.character_index = character_index
+        self.message = "{0} at character {1}".format(message, self.character_index + 1)
