51680d632e8eeb47535fcdb47271edda350b1316
Victor Engmark <victor.engmark@gmail.com>
2014-02-08 15:57:07 +0000

    Add character index in error message

--- vcard4/test_vcard4.py
+++ vcard4/test_vcard4.py
@@ -18,10 +18,15 @@ class TestRFC6350(unittest.TestCase):
         self.assertRaises(VcardError, validate_newlines, MINIMAL_VCARD_LINES_SEPARATED_BY_NL)
         self.assertRaises(VcardError, validate_newlines, MINIMAL_VCARD_LINES_SEPARATED_BY_NL_CR)
 
-    def test_individual_lines_within_vcard_are_delimited_by_the_rfc5322_line_break_error_message(self):
+    def test_individual_lines_within_vcard_are_delimited_by_the_rfc5322_line_break_error(self):
+        first_invalid_character = len(MINIMAL_VCARD_LINES[0]) + 1
         with self.assertRaises(VcardError) as error:
             validate_newlines(MINIMAL_VCARD_LINES_SEPARATED_BY_CR)
-        self.assertEqual(error.exception.message, "Invalid line delimiter")
+
+        self.assertEqual(
+            error.exception.message,
+            "Invalid line delimiter at character {0}".format(first_invalid_character))
+        self.assertEqual(error.exception.character, first_invalid_character)
 
 
 if __name__ == '__main__':
--- vcard4/vcard4.py
+++ vcard4/vcard4.py
@@ -4,13 +4,14 @@ INVALID_LINE_BREAK = re.compile(r"\n\r|\r(?!\n)|(?<!\r)\n")
 
 
 class VcardError(Exception):
-    def __init__(self, message):
+    def __init__(self, message, character):
         super(VcardError, self).__init__(message)
-        self.message = message
+        self.character = character
+        self.message = "{0} at character {1}".format(message, self.character)
 
 
 def validate_newlines(vcard_text):
     invalid_newlines = INVALID_LINE_BREAK.search(vcard_text)
     if invalid_newlines is not None:
-        raise VcardError("Invalid line delimiter")
+        raise VcardError("Invalid line delimiter", invalid_newlines.end())
     return True
