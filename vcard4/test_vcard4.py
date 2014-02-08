a17030394d65557b644801209e952155404877de
Victor Engmark <victor.engmark@gmail.com>
2014-02-08 14:36:51 +0000

    Raise exception when encountering invalid vCard content

--- vcard4/test_vcard4.py
+++ vcard4/test_vcard4.py
@@ -1,17 +1,27 @@
 import unittest
 
-from vcard4 import validate_newlines
+from vcard4 import validate_newlines, VcardError
+
 
 MINIMAL_VCARD_LINES = ["BEGIN:VCARD", "VERSION:4.0", "FN:Rene van der Harten", "END:VCARD"]
 RFC5322_LINE_BREAK = "\r\n"
+MINIMAL_VCARD_LINES_SEPARATED_BY_RFC5322_LINE_BREAK = RFC5322_LINE_BREAK.join(MINIMAL_VCARD_LINES)
+MINIMAL_VCARD_LINES_SEPARATED_BY_NL = "\n".join(MINIMAL_VCARD_LINES)
+MINIMAL_VCARD_LINES_SEPARATED_BY_CR = "\r".join(MINIMAL_VCARD_LINES)
+MINIMAL_VCARD_LINES_SEPARATED_BY_NL_CR = "\n\r".join(MINIMAL_VCARD_LINES)
 
 
 class TestRFC6350(unittest.TestCase):
     def test_individual_lines_within_vcard_are_delimited_by_the_rfc5322_line_break(self):
-        self.assertTrue(validate_newlines(RFC5322_LINE_BREAK.join(MINIMAL_VCARD_LINES)))
-        self.assertFalse(validate_newlines("\r".join(MINIMAL_VCARD_LINES)))
-        self.assertFalse(validate_newlines("\n".join(MINIMAL_VCARD_LINES)))
-        self.assertFalse(validate_newlines("\n\r".join(MINIMAL_VCARD_LINES)))
+        self.assertTrue(validate_newlines(MINIMAL_VCARD_LINES_SEPARATED_BY_RFC5322_LINE_BREAK))
+        self.assertRaises(VcardError, validate_newlines, MINIMAL_VCARD_LINES_SEPARATED_BY_CR)
+        self.assertRaises(VcardError, validate_newlines, MINIMAL_VCARD_LINES_SEPARATED_BY_NL)
+        self.assertRaises(VcardError, validate_newlines, MINIMAL_VCARD_LINES_SEPARATED_BY_NL_CR)
+
+    def test_individual_lines_within_vcard_are_delimited_by_the_rfc5322_line_break_error_message(self):
+        with self.assertRaises(VcardError) as error:
+            validate_newlines(MINIMAL_VCARD_LINES_SEPARATED_BY_CR)
+        self.assertEqual(error.exception.message, "Invalid line delimiter")
 
 
 if __name__ == '__main__':
--- vcard4/vcard4.py
+++ vcard4/vcard4.py
@@ -3,6 +3,14 @@ import re
 INVALID_LINE_BREAK = re.compile(r"\n\r|\r(?!\n)|(?<!\r)\n")
 
 
+class VcardError(Exception):
+    def __init__(self, message):
+        super(VcardError, self).__init__(message)
+        self.message = message
+
+
 def validate_newlines(vcard_text):
     invalid_newlines = INVALID_LINE_BREAK.search(vcard_text)
-    return invalid_newlines is None
+    if invalid_newlines is not None:
+        raise VcardError("Invalid line delimiter")
+    return True
