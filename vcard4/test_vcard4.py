a6c3bceee2b544fbc00c4ae16b2a98a91cac7aaf
Victor Engmark <victor.engmark@gmail.com>
2014-02-08 17:18:23 +0000

    Include line number in error

--- vcard4/test_vcard4.py
+++ vcard4/test_vcard4.py
@@ -19,13 +19,18 @@ class TestRFC6350(unittest.TestCase):
         self.assertRaises(VcardError, split_lines, MINIMAL_VCARD_LINES_SEPARATED_BY_REVERSED_LINE_BREAK)
 
     def test_individual_lines_within_vcard_are_delimited_by_the_rfc5322_line_break_error(self):
-        first_invalid_character_index = len(MINIMAL_VCARD_LINES[0])
+        invalid_vcard = MINIMAL_VCARD_LINES[0] + RFC5322_LINE_BREAK + "\r".join(MINIMAL_VCARD_LINES[1:])
+        first_invalid_line_index = 1
+        first_invalid_character_index = len(MINIMAL_VCARD_LINES[1])
         with self.assertRaises(VcardError) as error:
-            split_lines(MINIMAL_VCARD_LINES_SEPARATED_BY_CARRIAGE_RETURN)
+            split_lines(invalid_vcard)
 
         self.assertEqual(
             error.exception.message,
-            "Invalid line delimiter at character {0}".format(first_invalid_character_index + 1))
+            "Invalid line delimiter at line {0}, character {1}".format(
+                first_invalid_line_index + 1,
+                first_invalid_character_index + 1))
+        self.assertEqual(error.exception.line_index, first_invalid_line_index)
         self.assertEqual(error.exception.character_index, first_invalid_character_index)
 
 
--- vcard4/vcard4.py
+++ vcard4/vcard4.py
@@ -6,15 +6,16 @@ INVALID_LINE_BREAKS_MATCHER = re.compile("|".join(RFC5322_LINE_BREAK))
 
 def split_lines(text):
     lines = text.split(RFC5322_LINE_BREAK)
-    for line in lines:
-        invalid_newlines = INVALID_LINE_BREAKS_MATCHER.search(line)
+    for line_index in range(len(lines)):
+        invalid_newlines = INVALID_LINE_BREAKS_MATCHER.search(lines[line_index])
         if invalid_newlines is not None:
-            raise VcardError("Invalid line delimiter", invalid_newlines.start())
+            raise VcardError("Invalid line delimiter", line_index, invalid_newlines.start())
     return lines
 
 
 class VcardError(Exception):
-    def __init__(self, message, character_index):
+    def __init__(self, message, line_index, character_index):
         super(VcardError, self).__init__(message)
+        self.line_index = line_index
         self.character_index = character_index
-        self.message = "{0} at character {1}".format(message, self.character_index + 1)
+        self.message = "{0} at line {1}, character {2}".format(message, self.line_index + 1, self.character_index + 1)
