e77debb646e90dabb5932563c349ad1810656f6e
Victor Engmark <victor.engmark@gmail.com>
2014-02-08 16:35:13 +0000

    Rename and move code for readability

--- vcard4/test_vcard4.py
+++ vcard4/test_vcard4.py
@@ -6,27 +6,27 @@ from vcard4 import split_lines, VcardError
 MINIMAL_VCARD_LINES = ["BEGIN:VCARD", "VERSION:4.0", "FN:Rene van der Harten", "END:VCARD"]
 RFC5322_LINE_BREAK = "\r\n"
 MINIMAL_VCARD_LINES_SEPARATED_BY_RFC5322_LINE_BREAK = RFC5322_LINE_BREAK.join(MINIMAL_VCARD_LINES)
-MINIMAL_VCARD_LINES_SEPARATED_BY_NL = "\n".join(MINIMAL_VCARD_LINES)
-MINIMAL_VCARD_LINES_SEPARATED_BY_CR = "\r".join(MINIMAL_VCARD_LINES)
-MINIMAL_VCARD_LINES_SEPARATED_BY_NL_CR = "\n\r".join(MINIMAL_VCARD_LINES)
+MINIMAL_VCARD_LINES_SEPARATED_BY_CARRIAGE_RETURN = "\r".join(MINIMAL_VCARD_LINES)
+MINIMAL_VCARD_LINES_SEPARATED_BY_NEWLINE = "\n".join(MINIMAL_VCARD_LINES)
+MINIMAL_VCARD_LINES_SEPARATED_BY_REVERSED_LINE_BREAK = "\n\r".join(MINIMAL_VCARD_LINES)
 
 
 class TestRFC6350(unittest.TestCase):
     def test_individual_lines_within_vcard_are_delimited_by_the_rfc5322_line_break(self):
         self.assertEqual(split_lines(MINIMAL_VCARD_LINES_SEPARATED_BY_RFC5322_LINE_BREAK), MINIMAL_VCARD_LINES)
-        self.assertRaises(VcardError, split_lines, MINIMAL_VCARD_LINES_SEPARATED_BY_CR)
-        self.assertRaises(VcardError, split_lines, MINIMAL_VCARD_LINES_SEPARATED_BY_NL)
-        self.assertRaises(VcardError, split_lines, MINIMAL_VCARD_LINES_SEPARATED_BY_NL_CR)
+        self.assertRaises(VcardError, split_lines, MINIMAL_VCARD_LINES_SEPARATED_BY_CARRIAGE_RETURN)
+        self.assertRaises(VcardError, split_lines, MINIMAL_VCARD_LINES_SEPARATED_BY_NEWLINE)
+        self.assertRaises(VcardError, split_lines, MINIMAL_VCARD_LINES_SEPARATED_BY_REVERSED_LINE_BREAK)
 
     def test_individual_lines_within_vcard_are_delimited_by_the_rfc5322_line_break_error(self):
         first_invalid_character = len(MINIMAL_VCARD_LINES[0]) + 1
         with self.assertRaises(VcardError) as error:
-            split_lines(MINIMAL_VCARD_LINES_SEPARATED_BY_CR)
+            split_lines(MINIMAL_VCARD_LINES_SEPARATED_BY_CARRIAGE_RETURN)
 
         self.assertEqual(
             error.exception.message,
             "Invalid line delimiter at character {0}".format(first_invalid_character))
-        self.assertEqual(error.exception.character, first_invalid_character)
+        self.assertEqual(error.exception.character_index, first_invalid_character)
 
 
 if __name__ == '__main__':
--- vcard4/vcard4.py
+++ vcard4/vcard4.py
@@ -1,20 +1,20 @@
 import re
 
 RFC5322_LINE_BREAK = "\r\n"
-INVALID_LINE_BREAKS = re.compile("|".join(RFC5322_LINE_BREAK))
+INVALID_LINE_BREAKS_MATCHER = re.compile("|".join(RFC5322_LINE_BREAK))
+
+
+def split_lines(text):
+    lines = text.split(RFC5322_LINE_BREAK)
+    for line in lines:
+        invalid_newlines = INVALID_LINE_BREAKS_MATCHER.search(line)
+        if invalid_newlines is not None:
+            raise VcardError("Invalid line delimiter", invalid_newlines.end())
+    return lines
 
 
 class VcardError(Exception):
     def __init__(self, message, character):
         super(VcardError, self).__init__(message)
-        self.character = character
-        self.message = "{0} at character {1}".format(message, self.character)
-
-
-def split_lines(vcard_text):
-    lines = vcard_text.split(RFC5322_LINE_BREAK)
-    for line in lines:
-        invalid_newlines = INVALID_LINE_BREAKS.search(line)
-        if invalid_newlines is not None:
-            raise VcardError("Invalid line delimiter", invalid_newlines.end())
-    return lines
+        self.character_index = character
+        self.message = "{0} at character {1}".format(message, self.character_index)
